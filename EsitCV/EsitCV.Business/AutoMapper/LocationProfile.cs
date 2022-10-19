a054a8ce1e7d3434030a6f1e260953c7376fd89f
ysfeeazkl <77747662+ysfeeazkl@users.noreply.github.com>
2022-10-19 15:32:23 +0300

    LocationProfile

--- EsitCV/EsitCV.Business/AutoMapper/LocationProfile.cs
+++ EsitCV/EsitCV.Business/AutoMapper/LocationProfile.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using AutoMapper;
 using System.Threading.Tasks;
+using EsitCV.Entities.Concrete;
+using EsitCV.Entities.Dtos.LocationDtos;
 
 namespace EsitCV.Business.AutoMapper
 {
     public class LocationProfile : Profile
     {
+        public LocationProfile()
+        {
+            CreateMap<LocationAddDto, Location>();
+            CreateMap<LocationUpdateDto, Location>();
+        }
     }
 }
