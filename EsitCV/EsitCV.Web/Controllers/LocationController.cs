b7c2504932ae73266fe3365a9dbe77203e7af6ca
ysfeeazkl <77747662+ysfeeazkl@users.noreply.github.com>
2022-10-19 15:45:17 +0300

    Create LocationController.cs

---
+++ EsitCV/EsitCV.Web/Controllers/LocationController.cs
@@ -0,0 +1,81 @@
+﻿using EsitCV.Business.Abstract;
+using EsitCV.Entities.ComplexTypes;
+using EsitCV.Entities.Dtos.LocationDtos;
+using EsitCV.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EsitCV.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        readonly ILocationService _locationService;
+        public LocationController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+  
+
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddAsync(LocationAddDto jobPostingAddDto)
+        {
+            var result = await _locationService.AddAsync(jobPostingAddDto);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UpdateAsync(LocationUpdateDto jobPostingAddDto)
+        {
+            var result = await _locationService.UpdateAsync(jobPostingAddDto);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllAsync(bool? isDeleted, bool isAscending, int currentPage, int pageSize, OrderBy orderBy)
+        {
+            var result = await _locationService.GetAllAsync(isDeleted, isAscending, currentPage, pageSize, orderBy);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var result = await _locationService.GetByIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetByCompanyIdAsync(int id)
+        {
+            var result = await _locationService.GetByCompanyIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpDelete("[action]")]
+        public async Task<IActionResult> DeleteByIdAsync(int id)
+        {
+            var result = await _locationService.DeleteByIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpDelete("[action]")]
+        public async Task<IActionResult> HardDeleteByIdAsync(int id)
+        {
+            var result = await _locationService.HardDeleteByIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
