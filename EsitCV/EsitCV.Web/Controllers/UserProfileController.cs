c47b6668b22f241b1032290bc6003df336f728ab
Yusuf Efe Azaklı <77747662+ysfeeazkl@users.noreply.github.com>
2022-10-30 23:21:16 +0300

    UserProfileController

---
+++ EsitCV/EsitCV.Web/Controllers/UserProfileController.cs
@@ -0,0 +1,80 @@
+﻿using EsitCV.Business.Abstract;
+using EsitCV.Entities.ComplexTypes;
+using EsitCV.Entities.Dtos.UserPictureDtos;
+using EsitCV.Entities.Dtos.UserProfileDtos;
+using EsitCV.Shared.Utilities.Results.Abstract;
+using EsitCV.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EsitCV.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        IUserProfileService _userProfileService;
+
+        public UserProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddAsync(UserProfileAddDto userProfileAddDto)
+        {
+            var result = await _userProfileService.AddAsync(userProfileAddDto);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UpdateAsync(UserProfileUpdateDto userProfileUpdateDto)
+        {
+            var result = await _userProfileService.UpdateAsync(userProfileUpdateDto);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllAsync(bool? isDeleted, bool isAscending, int currentPage, int pageSize, OrderBy orderBy)
+        {
+            var result = await _userProfileService.GetAllAsync(isDeleted, isAscending, currentPage, pageSize, orderBy);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var result = await _userProfileService.GetByIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetByUserIdAsync(int id)
+        {
+            var result = await _userProfileService.GetByUserIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpDelete("[action]")]
+        public async Task<IActionResult> DeleteByIdAsync(int id)
+        {
+            var result = await _userProfileService.DeleteByIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+        [HttpDelete("[action]")]
+        public async Task<IActionResult> HardDeleteByIdAsync(int id)
+        {
+            var result = await _userProfileService.HardDeleteByIdAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
