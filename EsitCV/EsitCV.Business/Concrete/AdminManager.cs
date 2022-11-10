84c19280e5ce9cf3f3c8217fea32dd264c39ed1d
ysfeeazkl <77747662+ysfeeazkl@users.noreply.github.com>
2022-11-10 18:19:29 +0300

    admin classes create

---
+++ EsitCV/EsitCV.Business/Concrete/AdminManager.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using EsitCV.Business.Abstract;
+using EsitCV.Business.Utilities;
+using EsitCV.Data.Concrete.Context;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsitCV.Business.Concrete
+{
+    public class AdminManager:ManagerBase,IAdminService 
+    {
+
+        IHttpContextAccessor _httpContextAccessor;
+        public AdminManager(EsitCVContext context, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(mapper, context)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+    }
+}
