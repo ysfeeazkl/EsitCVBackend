1c0f77f232921f0b3bb3a54fdd7d36d5cad30e05
ysfeeazkl <77747662+ysfeeazkl@users.noreply.github.com>
2022-12-29 18:08:02 +0300

    company and user getDto

---
+++ EsitCV/EsitCV.Entities/Dtos/CompanyDtos/CompanyGetDto.cs
@@ -0,0 +1,27 @@
+﻿using EsitCV.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsitCV.Entities.Dtos.CompanyDtos
+{
+    public class CompanyGetDto
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string? IpAddress { get; set; }
+        public string Sector { get; set; }
+        public string TaxNumber { get; set; }
+
+        public DateTime YearOfFoundation { get; set; }
+        public DateTime LastLogin { get; set; }
+
+        public Location Location { get; set; }
+        public int LocationID { get; set; }
+        public CompanyPicture CompanyPicture { get; set; }
+        public int CompanyPictureID { get; set; }
+    }
+}
---
+++ EsitCV/EsitCV.Entities/Dtos/UserDtos/UserGetDto.cs
@@ -0,0 +1,28 @@
+﻿using EsitCV.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsitCV.Entities.Dtos.UserDtos
+{
+    public class UserGetDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserName { get; set; }
+        public string? IpAddress { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime LastLogin { get; set; }
+        public UserPicture UserPicture { get; set; }
+        public int UserPictureID { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public int UserProfileID { get; set; }
+        public CurriculumVitae CurriculumVitae { get; set; }
+        public int CurriculumVitaeID { get; set; }
+
+    }
+}
